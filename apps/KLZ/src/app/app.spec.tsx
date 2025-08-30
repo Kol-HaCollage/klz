5792b649df0bd8697321fdcab8726fdc5b4d8687
Davidi Shohat <davidishohat7@gmail.com>
2025-08-30 15:54:36 +0300

    ESLint changes derived from Antfu's config

--- apps/KLZ/src/app/app.spec.tsx
+++ apps/KLZ/src/app/app.spec.tsx
@@ -3,24 +3,24 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
 
-describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(baseElement).toBeTruthy();
-  });
+describe('app', () => {
+	it('should render successfully', () => {
+		const { baseElement } = render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>,
+		);
+		expect(baseElement).toBeTruthy();
+	});
 
-  it('should have a greeting as the title', () => {
-    const { getAllByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(
-      getAllByText(new RegExp('Welcome @klz/KLZ', 'gi')).length > 0
-    ).toBeTruthy();
-  });
+	it('should have a greeting as the title', () => {
+		const { getAllByText } = render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>,
+		);
+		expect(
+			getAllByText(/Welcome @klz\/KLZ/gi).length > 0,
+		).toBeTruthy();
+	});
 });
